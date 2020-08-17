101d9818340d3933dd17b0dceb06089ae2522e9e
Nathaniel Thorn <nthorn@riskalyze.com>
2020-08-16 17:47:39 -0700

    Setting up initial ui

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './App.css'
+
+const App: React.StatelessComponent<{}> = () => (
+    <div className='container'>
+        <h1>Welcome to Bar Keep</h1>
+    </div>
+)
+
+export default App;

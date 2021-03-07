a4ffe315c220a8078ac9b090d540675f2e6bd446
Nathaniel Thorn <nthorn@riskalyze.com>
2021-03-07 13:56:21 -0800

    MVP for fetching recipes based on ingredients

--- src/components/BarMenu.tsx
+++ src/components/BarMenu.tsx
@@ -5,7 +5,7 @@ import Box from '@material-ui/core/Box';
 import backBackApi from '../services/barBackApi';
 import InventoryDisplay from './InventoryDisplay';
 import Product from '../types/Product';
-import Inventory from '../types/Inventory';
+import Inventory, { InventoryPriority } from '../types/Inventory';
 import AddNewInventory from './AddNewInventory';
 import Recipe from '../types/Recipe';
 
@@ -53,30 +53,48 @@ class BarKeep extends React.Component<BarMenuProps, BarMenuState> {
     };
   }
 
+  getProductIdListByInventoryPriority(priority: InventoryPriority) {
+    return this.state.availableInventory.reduce((requiredProductList, inventoryItem) => {
+      if (inventoryItem.priority == priority) {
+        requiredProductList.push(inventoryItem.product.id);
+      }
+      return requiredProductList;
+    }, []);
+  }
+
   componentDidUpdate() {
-    console.log(this.state, this.props);
-
-
-    if (this.props.inventory.length !== this.state.availableInventory.length){ //TODO: will need to actually compare items in inventory
-        console.log('in')
-      
+    if (this.props.inventory.length !== this.state.availableInventory.length) { //TODO: will need to actually compare items in inventory
       this.setState({ ...this.state, availableInventory: this.props.inventory, menuState: MenuState.LOADING }, () => {
-        backBackApi.get(`/recipes`)
+        backBackApi.get(`/recipes`, {
+          params: {
+            requiredProducts: this.getProductIdListByInventoryPriority(InventoryPriority.REQUIRED),
+            availableProducts: this.getProductIdListByInventoryPriority(InventoryPriority.AVAILABLE)
+          }
+        })
           .then(res => {
             this.setState({ ...this.state, availableMenu: res.data, menuState: MenuState.READY });
           }).catch(err => {
             this.setState({ ...this.state, availableMenu: [], menuState: MenuState.ERROR });
           })
       });
-    } else {
-      console.log('dont recalc')
     }
   }
 
   render() {
+    let menuDisplay;
+    if (this.state.menuState == MenuState.READY) {
+      menuDisplay = this.state.availableMenu.map(menuRecipe => {
+        return <div key={menuRecipe.id}>{menuRecipe.name}</div>
+      })
+    } else if (this.state.menuState === MenuState.LOADING) {
+      menuDisplay = <div>Refreshing menu...</div>
+    } else {
+      menuDisplay = <div>Uh oh, something went wrong!</div>
+    }
+
     return (
       <Container>
-        test
+        {menuDisplay}
       </Container>
     )
   }
